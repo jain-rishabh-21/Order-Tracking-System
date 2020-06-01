23f9256a019644078d7154c5d260a59455fc358b
kackerzzz <rishabhee21@gmail.com>
2020-06-02 04:17:04 +0530

    Collection Manupulation

--- src/main/java/com/pluralsight/speldemo/data/Order.java
+++ src/main/java/com/pluralsight/speldemo/data/Order.java
@@ -3,6 +3,9 @@ package com.pluralsight.speldemo.data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+
 @Component("order")
 public class Order {
 
@@ -12,7 +15,7 @@ public class Order {
     @Value("#{order.amount >= 1000 ? order.ammount * 5 / 100 : 0}")
     private double discount;
 
-    @Value("#{user.country == 'India' and user.timeZone == 'Asia/Kolkata' ? 3 : 14}")
+    @Value("#{user.country == 'US' and user.timeZone == 'Americal/NewYork' ? 3 : 14}")
     private int daysToDeliver;
 
     @Value("#{user.country}")
@@ -20,6 +23,19 @@ public class Order {
 
     @Value("#{T(java.text.NumberFormat).getCurrencyInstance(T(java.util.Locale).getDefault()).format(order.amount)}")
     private String formattedAmount;
+
+    @Value("#{shipping.locationsByCountry[user.country]}")
+    private List<City> shippingLocations;
+
+    @Value("#{order.shippingLocations.?[isCapital != true]}")
+    private List<City> nonCapitalshippingLocations;
+
+    @Value("#{(shipping.locationsByCountry.?[key == 'US' || key == 'UK'])}")
+    private Map<String, List<City>> westernShippingLocation;
+
+    @Value("#{(shipping.locationsByCountry.?[shipping < 10].size())}")
+    private Integer cheapShippingLocations;
+
 
     public double getAmount() {
         return amount;
@@ -61,4 +77,35 @@ public class Order {
         this.formattedAmount = formattedAmount;
     }
 
+    public List<City> getShippingLocations() {
+        return shippingLocations;
+    }
+
+    public void setShippingLocations(List<City> shippingLocations) {
+        this.shippingLocations = shippingLocations;
+    }
+
+    public List<City> getNonCapitalshippingLocations() {
+        return nonCapitalshippingLocations;
+    }
+
+    public void setNonCapitalshippingLocations(List<City> nonCapitalshippingLocations) {
+        this.nonCapitalshippingLocations = nonCapitalshippingLocations;
+    }
+
+    public Map<String, List<City>> getWesternShippingLocation() {
+        return westernShippingLocation;
+    }
+
+    public void setWesternShippingLocation(Map<String, List<City>> westernShippingLocation) {
+        this.westernShippingLocation = westernShippingLocation;
+    }
+
+    public Integer getCheapShippingLocations() {
+        return cheapShippingLocations;
+    }
+
+    public void setCheapShippingLocations(Integer cheapShippingLocations) {
+        this.cheapShippingLocations = cheapShippingLocations;
+    }
 }
